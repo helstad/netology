baf12ae813de89de2bd1fc4c41a5e46b37c68639
Syzygy <saf-ivan@yandex.ru>
2023-09-26 22:06:35 +0300

    Change task_2

--- lesson_2/task_2/main.cpp
+++ lesson_2/task_2/main.cpp
@@ -12,25 +12,25 @@ void change_balance(bank_account_info& account, double new_balance) {
 }
 
 int main() {
-    bank_account_info info;
+    bank_account_info account;
 
     std::cout << "Enter bank account: ";
-    std::cin >> info.bank_account;
+    std::cin >> account.bank_account;
     std::cin.ignore();
 
     std::cout << "Enter name: ";
-    std::getline(std::cin, info.name);
+    std::getline(std::cin, account.name);
 
     std::cout << "Enter balance: ";
-    std::cin >> info.balance;
+    std::cin >> account.balance;
 
     double new_balance;
     std::cout << "Enter new balance: ";
     std::cin >> new_balance;
 
-    change_balance(info, new_balance);
+    change_balance(account, new_balance);
 
-    std::cout << "Your account: " << info.name << ", " << info.bank_account << ", " << info.balance << std::endl;
+    std::cout << "Your account: " << account.name << ", " << account.bank_account << ", " << account.balance << std::endl;
 
     return EXIT_SUCCESS;
 }
