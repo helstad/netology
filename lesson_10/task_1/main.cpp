c12ce07e46d145e42c74394b9ed820bb374c913a
Syzygy <saf-ivan@yandex.ru>
2023-10-21 20:58:35 +0300

    add task_1

---
+++ lesson_10/task_1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::string name;
+
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!";
+    return 0;
+}
