ab4f18425e2c9ead178413e6fd7a870666a5a871
helstad <saf-ivan@yandex.ru>
2024-02-28 23:00:39 +0300

    fix CMakeLists

--- sqlcpp/lesson_5/task_1/main.cpp
+++ sqlcpp/lesson_5/task_1/main.cpp
@@ -1,24 +1,9 @@
 #include <iostream>
 #include <pqxx/pqxx>
 
+
 int main() {
-    std::cout << "Hello PostgreSQL" << std::endl;
+  std::cout << "Hello" << std::endl;
 
-    try
-    {
-        pqxx::connection(
-            "host=127.0.0.1 "
-            "port=5432 "
-            "dbname=dvdrental "
-            "user=postgres "
-            "password=Ohabop56"
-        );
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
-
-    return 0;
+  return 0;
 }
