5e69bbbf2b2bbe3383331000e68d8d6b46c6f246
helstad <saf-ivan@yandex.ru>
2024-04-13 20:20:11 +0300

    add lesson_5

---
+++ thread/lesson_5/task_2/mainClang.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello Docker Clang!" << std::endl;
+	return 0;
+}
---
+++ thread/lesson_5/task_2/mainGCC.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello Docker GCC!" << std::endl;
+	return 0;
+}
